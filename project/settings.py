6e35b70ec24acf33b48fdf24cb18ed694f61b9fb
Mike Salter <mikey.l.salter@gmail.com>
2022-01-21 14:12:48 +0000

    installs

--- project/settings.py
+++ project/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pawhub-db',
+        'HOST': 'localhost',
+        'Port': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
